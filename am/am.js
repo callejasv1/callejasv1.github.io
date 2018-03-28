af0b69db2b2ca6fc1dd138107e2b2f6488b7a777
callejasv1 <37317495+callejasv1@users.noreply.github.com>
2018-03-28 19:43:42 -0300

    Update am.js
    Se agregan comentarios

--- am/am.js
+++ am/am.js
@@ -1,4 +1,4 @@
-// ejemplo clase 3
+//Proyecto RE_Cover, AM by Arctic Monkeys 
 // Eduardo Callejas
 
 var x = 0;
@@ -12,17 +12,18 @@ createCanvas(600,600);
 
 function draw(){
   background(0);
-var frec=height/40
+var frec=height/40 //número de ondas en panalla
 var posx;
 var posy;
 for(var i=0;i<=600;i++){
 var theta = map(i,0,600,0,4*PI);
- ampsen=1/2*(sin((theta)-PI/2))+1;
-amplitud=mouseY/300*ampsen*height/10;
+ ampsen=1/2*(sin((theta)-PI/2))+1; //la amplitud de las ondas tambien es una funcion de seno
+  
+amplitud=mouseY/300*ampsen*height/10; // agrega interacción con la posición del mouse
 
 stroke(255);
 strokeWeight(3);
-if(i>1){
+if(i>1){ // en i=0 se crea el primer punto, desde i=1 comienzan a dibujarse las lineas
 
 line(posx,posy,i,height/2+amplitud*sin(PI/frec*i+PI/frec*y));}
 //point(i,height/2+amplitud*sin(PI/frec*i+PI/frec*y));
@@ -30,7 +31,7 @@ posx= i;
 posy= height/2+amplitud*sin(PI/frec*(i)+PI/frec*y);
 }
 
-y=y+1;
+y=y+1; //no me acuerdo porque es esto
 //dibujarseno();
 
 }
